910741b72a3741db34e4ed3f82ff1e07f3c8fc9b
Md Hasanujjaman <76647218+vintage-alien@users.noreply.github.com>
2022-09-18 12:47:01 +0530

    Delete Python Course Lex directory

--- Python Course Lex/list1.py
+++
@@ -1,8 +0,0 @@
-list_of_airlines=["AL1","AL2","AL3"]
-print("Iterating the list using range()")
-for index in range(0,len(list_of_airlines)):
-    print(list_of_airlines[index])
-    
-print("Iterating the list using keyword in")
-for airline in list_of_airlines:
-    print(airline)
--- Python Course Lex/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-print("Iterating the string using range()")
-for index in range(0,len(pancard_number)):
-    print(pancard_number[index])
